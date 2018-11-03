9aa0088cc4742c0d9a598d087fee3820c0da1e09
Alexander Veselov <Alexander_Veselov@yahoo.com>
2018-11-03 22:55:48 +0300

    Complete Prolog Task 1

--- exercises/prolog/1_simple_predicates.pl
+++ exercises/prolog/1_simple_predicates.pl
@@ -10,8 +10,35 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
+
+	brother(X, Y) :- father(Z, X), father(Z, Y), X \= Y.
+	cousin(X, Y) :- brother(Z1, Z2), father(Z1, X), father(Z2, Y), X \= Y.
+	grandson(X, Y) :- father(Y, Z), father(Z, X).
+	descendent(X, Y) :- father(Y, X).
+	descendent(X, Y) :- father(Y, Z), descendent(X, Z).
+
+	:- initialization
+		writeln("brother(X, Y):"),
+		forall(brother(X, Y), write([X, Y])),
+
+		writeln("\ncousin(X, Y):"),
+		forall(cousin(X, Y), write([X, Y])),
+
+		writeln("\ngrandson(X, Y):"),
+		forall(grandson(X, Y), write([X, Y])),
+
+		writeln("\ndescendent(X, Y):"),
+		forall(descendent(X, Y), write([X, Y])),
+
+		halt.
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+
+% brother(X, Y):
+% [b,c][c,b][d,e][e,d]
+% cousin(X, Y):
+% [d,f][e,f][f,d][f,e]
+% grandson(X, Y):
+% [d,a][e,a][f,a]
+% descendent(X, Y):
+% [b,a][c,a][d,b][e,b][f,c][d,a][e,a][f,a]
