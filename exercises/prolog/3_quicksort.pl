4717d786406a1eb4a52350d1134e76fe937df5aa
Igor Drozdov <igor.drozdov@iii.com>
2018-11-10 12:24:41 +0300

    quicksort done

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,20 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+quicksort([X|Xs],Ys) :-
+  partition(Xs,X,Left,Right),
+  quicksort(Left,Ls),
+  quicksort(Right,Rs),
+  append(Ls,[X|Rs],Ys).
+quicksort([],[]).
+
+partition([X|Xs],Y,[X|Ls],Rs) :-
+  X =< Y, partition(Xs,Y,Ls,Rs).
+partition([X|Xs],Y,Ls,[X|Rs]) :-
+  X > Y, partition(Xs,Y,Ls,Rs).
+partition([],Y,[],[]).
+
+append([],Ys,Ys).
+append([X|Xs],Ys,[X|Zs]) :- append(Xs,Ys,Zs).
+
+%quicksort([2,3,5,1,3,5,62,2,34,4,5], K).
+%K = [1, 2, 2, 3, 3, 4, 5, 5, 5|...] .
