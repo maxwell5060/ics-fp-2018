c502adafdbc6dc3bd38c8473fda300b673a0b901
denis-koptev <koptevda@ya.ru>
2018-11-12 00:50:27 +0300

    [PROLOG] truth_table task completed

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,58 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(fail).
+
+and(A, B):- A, B.
+or(A, B):- A; B.
+not(A):- \+A.
+equ(A, B):- A = B.
+xor(A, B):- not(equ(A, B)).
+
+calc(E, true):- E, !.
+calc(_, fail).
+
+truth_table(A, B, E):- bool(A), bool(B), write(A), write(' '), write(B), write(' '), calc(E, Res), writeln(Res), fail.
+truth_table(_,_,_):- nl, true.
+
+:- writeln("AND:").
+:- truth_table(A, B, and(A,B)).
+
+:- writeln("OR:").
+:- truth_table(A, B, or(A,B)).
+
+:- writeln("XOR:").
+:- truth_table(A, B, xor(A,B)).
+
+:- writeln("EQU:").
+:- truth_table(A, B, equ(A,B)).
+
+/* Results:
+
+AND:
+true true true
+true fail fail
+fail true fail
+fail fail fail
+
+OR:
+true true true
+true fail true
+fail true true
+fail fail fail
+
+XOR:
+true true fail
+true fail true
+fail true true
+fail fail fail
+
+EQU:
+true true true
+true fail fail
+fail true fail
+fail fail true
+
+*/
