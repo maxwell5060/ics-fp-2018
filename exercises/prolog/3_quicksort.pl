9698592f93b89e1aff852e00fdb7f761719e43ed
antonnazarov <nazarof.anton@gmail.com>
2018-11-12 13:32:18 +0300

    prolog 2 3 5

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,19 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+quicksort([H|T],X):-
+	partition(H,T,Left,Right),
+	quicksort(Left,S1),
+	quicksort(Right,S2),
+	append(S1,[H|S2],X).
+
+quicksort([],[]).
+
+partition(Pivot,[H|T],[H|Rest],RightSide):-
+	H =< Pivot,
+	partition(Pivot,T,Rest,RightSide).
+
+partition(Pivot,[H|T],LeftSide,[H|Rest]):-
+	H > Pivot,
+	partition(Pivot,T,LeftSide,Rest).
+
+partition(_,[],[],[]).
--- exercises/prolog/5_merge.pl
+++ exercises/prolog/5_merge.pl
@@ -1,2 +1,30 @@
 $ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
 % определяет список R, составленный из этих элементов
+
+first([H|_], H).
+
+split(A, B, C) :-
+    append(B, C, A), length(B, L1), length(C, L2), L1 = L2.
+split(A, B, C) :-
+    append(B, C, A), length(B, L1), length(C, L2), L3 is L2+1, L1 = L3.
+
+
+merge([], R, R).
+merge(L, [], L).
+
+merge([L1|L2], R, B) :-
+    first(R, R1), L1 =< R1, merge(L2, R, C), append([L1], C, B).
+merge(L, [R1|R2], B) :-
+    first(L, L1), R1 < L1, merge(R2, L, C), append([R1], C, B).
+
+
+mergesort([H|[]], [H]).
+
+mergesort(A, B) :-
+    length(A, Len), Len > 1,
+    split(A, L, R),
+    mergesort(L, SL), mergesort(R, SR),
+    merge(SL, SR, B).
+
+% merge([-1,1,4,5],[-3,0,2,3,5],L)
+% L = [-3, -1, 0, 1, 2, 3, 4, 5, 5]
