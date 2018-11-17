6a00629d61a225fd9cacea2c8516fe726fd6b1a0
Rodion <i.rodion.r@gmail.com>
2018-11-17 21:38:14 +0300

    Add prolog solution 1

--- exercises/prolog/1_simple_predicates.pl
+++ exercises/prolog/1_simple_predicates.pl
@@ -11,7 +11,35 @@
 	father(b,e).  % 4
 	father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+%	?- brother(X,Y).
+%	?- cousin(X,Y).
+%	?- grandson(X,Y).
+%	?- descendent(X,Y).
+
+brother(X,Y) :- father(Z,X), father(Z,Y), X \= Y.
+cousin(X,Y) :- father(Z1,X), father(Z2,Y), brother(Z1,Z2).
+grandson(X,Y) :- father(Z,X), father(Y,Z).
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(Y,Z), descendent(X,Z).
+
+:-
+write("brother(X,Y): "),
+forall(brother(X,Y), (write([X,Y]))),
+write("\ncousin(X,Y): "),
+forall(cousin(X,Y), write([X,Y])),
+write("\ngrandson(X,Y): "),
+forall(grandson(X,Y), write([X,Y])),
+write("\ndescendent(X,Y): "),
+forall(descendent(X,Y), write([X,Y])),
+writeln("\n").
+
+%     a
+%    / \
+%   b   c
+%  / \   \
+% d   e   f
+
+% brother(X,Y): [b,c][c,b][d,e][e,d]
+% cousin(X,Y): [d,f][e,f][f,d][f,e]
+% grandson(X,Y): [d,a][e,a][f,a]
+% descendent(X,Y): [b,a][c,a][d,b][e,b][f,c][d,a][e,a][f,a]
