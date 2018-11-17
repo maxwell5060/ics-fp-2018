6e1dab71ca5f96143132a6211d975ac3ec3f2263
raccoonattack <attackraccoon@gmail.com>
2018-11-17 22:13:03 +0300

    Added prolog task 2

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,68 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(fail).
+
+and(A, B):- A, B.
+or(A, B):- A; B.
+not(A):- \+A.
+eq(A, B):- A = B.
+xor(A, B):- not(eq(A, B)).
+
+eval(Expr, true):- Expr, !.
+eval(_, fail).
+
+truth_table(A, B, Expr):- bool(A), bool(B), eval(Expr, Result), writef('%t %t %t\n', [A, B, Result]), fail.
+truth_table(_, _, _):- true.
+
+:- writeln("AND truth table\n").
+:- truth_table(A, B, and(A, B)).
+
+:- writeln("\nOR truth table\n").
+:- truth_table(A, B, or(A, B)).
+
+:- writeln("\nXOR truth table\n").
+:- truth_table(A, B, xor(A, B)).
+
+:- writeln("\nEQ truth table\n").
+:- truth_table(A, B, eq(A, B)).
+
+:- writeln("\nA AND (A OR B) truth table\n").
+:- truth_table(A, B, and(A, or(A, B))).
+
+% AND truth table
+
+% true true true
+% true fail fail
+% fail true fail
+% fail fail fail
+
+% OR truth table
+
+% true true true
+% true fail true
+% fail true true
+% fail fail fail
+
+% XOR truth table
+
+% true true fail
+% true fail true
+% fail true true
+% fail fail fail
+
+% EQ truth table
+
+% true true true
+% true fail fail
+% fail true fail
+% fail fail true
+
+% A AND (A OR B) truth table
+
+% true true true
+% true fail true
+% fail true fail
+% fail fail fail
