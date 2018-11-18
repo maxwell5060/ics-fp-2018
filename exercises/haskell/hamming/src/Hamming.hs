6937777938c322ee7ab432cb13a24f5807e2e5d9
Roman <churomann@gmail.com>
2018-11-19 02:26:37 +0300

    Add haskell task (hamming)

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,20 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys
+    | len_xs == len_ys = Just dist
+    | otherwise = Nothing
+    where
+        initDist = 0
+        len_xs = length xs
+        len_ys = length ys
+        dist = hamming xs ys initDist
+
+hamming :: String -> String -> Int -> Int
+hamming [] [] d = d
+hamming (x:xs) (y:ys) d
+    | x /= y = incrHamming
+    | otherwise = currHamming
+    where
+        incrHamming = hamming xs ys d+1
+        currHamming = hamming xs ys d
