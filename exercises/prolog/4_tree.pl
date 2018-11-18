89b7d6eaf58031a3c207879692a0a4471bb5a9b4
antonnazarov <nazarof.anton@gmail.com>
2018-11-18 15:35:05 +0300

    prolog 4

--- exercises/prolog/4_tree.pl
+++ exercises/prolog/4_tree.pl
@@ -3,3 +3,43 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+quicksort([H|T],X):-
+	partition(H,T,Left,Right),
+	quicksort(Left,S1),
+	quicksort(Right,S2),
+	append(S1,[H|S2],X).
+
+quicksort([],[]).
+
+partition(Pivot,[H|T],[H|Rest],RightSide):-
+	H =< Pivot,
+	partition(Pivot,T,Rest,RightSide).
+
+partition(Pivot,[H|T],LeftSide,[H|Rest]):-
+	H > Pivot,
+	partition(Pivot,T,LeftSide,Rest).
+
+partition(_,[],[],[]).
+
+divide(List, L, R) :-
+    append(L, R, List),
+    length(L, LL),
+    length(R, RL),
+    (LL =:= RL; LL =:= (RL - 1)), !.
+
+balanced_tree([], empty).
+balanced_tree(List, instant(H, empty, empty)) :- 
+    List = [H|[]], !.
+balanced_tree([Lo|[Hi|[]]], instant(Hi, LNode, empty)) :- 
+    Lo < Hi, 
+    balanced_tree([Lo], LNode), !.
+balanced_tree([Hi|[Lo|[]]], instant(Hi, LNode, empty)) :- 
+    balanced_tree([Lo], LNode), !.
+balanced_tree(List, instant(Mid, LNode, RNode)) :-
+    quicksort(List, Sorted),
+    divide(Sorted, LeftList, [Mid|RightList]),
+    balanced_tree(LeftList, LNode),
+    balanced_tree(RightList, RNode), !.
+% balanced_tree([9,3,6,7,2,1], T)
+% T = instant(6, instant(2, instant(1, empty, empty), instant(3, empty, empty)), instant(9, instant(7, empty, empty), empty))
