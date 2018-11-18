76f8fcee360b7e4b620a0eb994edbec34396878e
Rodion <i.rodion.r@gmail.com>
2018-11-18 15:16:57 +0300

    Add prolog solution 3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,16 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+part(_, [], [], []).
+part(P, [H|T], [H|L], R) :- P > H, part(P, T, L, R).
+part(P, [H|T], L, [H|R]) :- part(P, T, L, R).
+
+qsort([], []).
+qsort([H|T], S) :- part(H, T, L, R), qsort(L, SL), qsort(R, SR), append(SL, [H|SR], S).
+
+:-
+write("qsort([0,9,1,8,2,7,5,5,5], A): "),
+qsort([0,9,1,8,2,7,5,5,5], A),
+writeln(A),
+writeln("").
+
+% qsort([0,9,1,8,2,7,5,5,5], A): [0,1,2,5,5,5,7,8,9]
