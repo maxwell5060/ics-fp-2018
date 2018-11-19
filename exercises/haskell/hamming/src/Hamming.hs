80671b046c94b7c050fe9790be6daac3c4573fe0
Sergey Mishin <sergei.a.mishin@gmail.com>
2018-11-19 05:14:34 +0300

    Add haskell solution

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,15 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys =
+  if (length xs) /= (length ys)
+  then Nothing
+  else Just (distance' xs ys 0)
+
+-- distance' gets validated data, e.g. lists of the same length
+distance' :: String -> String -> Int -> Int
+distance' [] [] acc = acc
+distance' (x:xs) (y:ys) acc =
+  if x == y
+  then distance' xs ys acc
+  else distance' xs ys (1 + acc)
