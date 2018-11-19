9102e632dd7a015318792800a19a8fe5e8fd2468
Sergey Mishin <sergei.a.mishin@gmail.com>
2018-11-19 06:04:05 +0300

    Update haskell solution

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,7 +1,16 @@
-module Hamming (distance) where
+module Hamming (distance, distance2) where
 
 distance :: String -> String -> Maybe Int
 distance xs ys =
+  if (length xs) /= (length ys)
+  then Nothing
+  else Just (length $ filter (\(x, y) -> x /= y) (zip xs ys))
+
+-- Old solution
+
+-- distance2 + distance' is also works, but it is just huge
+distance2 :: String -> String -> Maybe Int
+distance2 xs ys =
   if (length xs) /= (length ys)
   then Nothing
   else Just (distance' xs ys 0)
@@ -13,3 +22,4 @@ distance' (x:xs) (y:ys) acc =
   if x == y
   then distance' xs ys acc
   else distance' xs ys (1 + acc)
+
