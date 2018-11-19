9d21ca31021907880d06adee569440f913aa5463
Duckling Dark <paul.panda.969@gmail.com>
2018-11-19 20:12:10 +0300

    Correct files location

---
+++ exercises/golang/exercise/sample.go
@@ -0,0 +1,13 @@
+package exercise
+
+import (
+	"github.com/dikderoy/imagen/drawer"
+	log "github.com/sirupsen/logrus"
+	"image/color"
+)
+
+
+func NewMandelbrot(iterations int, scaleFactor float32, offsetX, offsetY float32) drawer.MandelbrotGenerator {
+	//start here
+}
+
