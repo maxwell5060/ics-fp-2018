48fec43e5cca46daf845a81908c73d1f9ac997a6
Sergey Mishin <sergei.a.mishin@gmail.com>
2018-11-19 03:39:58 +0300

    Add prolog 2 solution

--- prolog/2_truth_table.pl
+++ prolog/2_truth_table.pl
@@ -10,3 +10,26 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+not(A) :- A = fail.
+equ(A, B) :- A = B.
+xor(A, B) :- and(A, not(B)); and(B, not(A)).
+
+
+%% find(Symbol) matches symbol to true or to fail
+find(true).
+find(fail).
+
+%% find(Expression, Res), matches the result of expression, same as above
+find(Expr, true) :- Expr, !. % ! to stop finding
+find(_, fail).
+
+truth_table(A, B, Expr) :-
+    %% find values at the top to prevent inconsistent backtracking
+    find(A), find(B), find(Expr, Res),
+    %% write a row
+    write(A), write(' '), write(B), write(' '), write(Res), nl,
+    %% return fail to find other solutions
+    fail.
