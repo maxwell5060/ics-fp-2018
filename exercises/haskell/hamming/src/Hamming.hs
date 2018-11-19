71c2b22860d7c20df135a717846979b166859ba7
Max Elkin <maksim.elkin@yahoo.com>
2018-11-19 22:11:29 +0300

    Complete Haskell task

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,12 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = compare 0 xs ys
+	where
+		compare n [] [] = Just n
+		compare n [] ys = Nothing
+		compare n xs [] = Nothing
+		compare n (head1:tail1) (head2:tail2) =
+			if head1 /= head2
+				then compare (n+1) tail1 tail2
+				else compare n tail1 tail2
