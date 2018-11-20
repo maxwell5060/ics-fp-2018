30c73c53a4b100c6b306c6f4545333af3d9357d2
kopted <Denis.Koptev@emc.com>
2018-11-20 13:24:03 +0300

    [PROLOG] quicksort task completed

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,29 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+divide(_, [], [], []).
+
+divide(Sep, [Head|Tail], [Head|Left], Right):-
+	Sep >= Head,
+	divide(Sep, Tail, Left, Right).
+
+divide(Sep, [Head|Tail], Left, [Head|Right]):-
+	divide(Sep, Tail, Left, Right).
+
+
+qsort([], []).
+qsort(L, K):-
+	L = [Head|Tail],
+	divide(Head, Tail, Left, Right),
+	qsort(Left, SLeft),
+	qsort(Right, SRight),
+	append(SLeft, [Head|SRight], K).
+
+
+/*
+
+Results:
+
+?- qsort([3,2,-10,4,5,9,3,1],R).
+R = [-10, 1, 2, 3, 3, 4, 5, 9]
+
+*/
