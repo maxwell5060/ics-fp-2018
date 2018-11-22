bbcde166df4a665404d217a40e89b9f3c97af1bd
rogeeNok <rogee.nok@gmail.com>
2018-11-23 00:51:12 +0300

    prolog task 1 impl

--- exercises/prolog/1_simple_predicates.pl
+++ exercises/prolog/1_simple_predicates.pl
@@ -10,8 +10,47 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
-% указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+
+% правила родословной
+brother(X,Y) :- father(Z,X), father(Z,Y), X \= Y.
+cousin(X,Y) :- father(Z,X), father(W,Y), brother(Z,W).
+grandson(X,Y) :- father(Y,W), father(W,X).
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(Y,W), descendent(X,W).
+	
+% настала пора узнать, кто есть кто!
+:- writeln('Bros:').
+:- forall(brother(X,Y), (write(X), write(' and '), write(Y), nl)).
+:- writeln('Cousins:').
+:- forall(cousin(X,Y), (write(X), write(' and '), write(Y), nl)).
+:- writeln('Grandsons:').
+:- forall(grandson(X,Y), (write(X), write(' to '), write(Y), nl)).
+:- writeln('Descendents:').
+:- forall(descendent(X,Y), (write(X), write(' of '), write(Y), nl)).
+
+% ответы таковы
+/*
+Bros:
+b and c
+c and b
+d and e
+e and d
+Cousins:
+d and f
+e and f
+f and d
+f and e
+Grandsons:
+d to a
+e to a
+f to a
+Descendents:
+b of a
+c of a
+d of b
+e of b
+f of c
+d of a
+e of a
+f of a
+*/
