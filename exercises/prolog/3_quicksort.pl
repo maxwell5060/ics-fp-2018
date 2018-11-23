38a72cafed65573d353fb9697278ec7e053d8abe
rogeeNok <rogee.nok@gmail.com>
2018-11-23 17:23:00 +0300

    prolog task 3 impl

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,22 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort(L, K) :-
+  L = [Head|Tail],
+  pivot(Head,Tail,Left,Right),
+  qsort(Left,LeftSorted),
+  qsort(Right,RightSorted),
+  append(LeftSorted,[Head|RightSorted],K).
+qsort([],[]).
+
+pivot(X,[Head|Tail],[Head|Ls],Rs) :- 
+  X > Head, pivot(X,Tail,Ls,Rs).
+pivot(X,[Head|Tail],Ls,[Head|Rs]) :- 
+  pivot(X,Tail,Ls,Rs).
+pivot(_,[],[],[]).
+
+
+/* Example of usage
+
+?- qsort([5,0,11,13,12,-2,0,59,2],R).
+R = [-2, 0, 0, 2, 5, 11, 12, 13, 59]
+*/
