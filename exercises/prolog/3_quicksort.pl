87d37c48bbcfcaef8c618082b2492587e2ed333a
raccoonattack <attackraccoon@gmail.com>
2018-11-25 16:16:03 +0300

    Added prolog task 3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,20 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+partition(_, [], [], []).
+
+partition(Pivot, [Head|Tail], [Head|LeftPart], RightPart):- 
+	Pivot >= Head, partition(Pivot, Tail, LeftPart, RightPart).
+
+partition(Pivot, [Head|Tail], LeftPart, [Head|RightPart]):- 
+	partition(Pivot, Tail, LeftPart, RightPart).
+
+qsort([], []).
+
+qsort([Head|Tail], K):-
+	partition(Head, Tail, LeftPart, RightPart),
+	qsort(LeftPart, SortedLeftPart),
+	qsort(RightPart, SortedRightPart),
+	append(SortedLeftPart, [Head|SortedRightPart], K).
+
+:- qsort([1, 2, 3, -4, 6, 7, 0, 3, 4, -1], R), write("R = "), writeln(R).
+% R = [-4,-1,0,1,2,3,3,4,6,7]
