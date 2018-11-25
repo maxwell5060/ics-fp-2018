8c9f14bdc78be18d5837490913902fe5fa06233b
Sempai Fedya <44314758+FedorPolevoy@users.noreply.github.com>
2018-11-25 13:14:06 +0300

    2 task truth table

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,22 @@
 % true fail true
 % fail true fail
 % fail fail fail
+	
+	bool(true).
+	bool(fail).
+	
+	and(A, B) :- A, B.
+	or(A, B) :- A; B.
+	not(A) :- \+A.
+	equ(A, B) :- A = B.
+	xor(A, B) :- not(equ(A, B)).
+	
+	eval(Expression) :- call(Expression), writeln("true"), !.
+ 	eval(_) :- writeln("fail"), !.
+
+	truth_table(A, B, Expression) :- bool(A), bool(B),
+  write(A), write(' '),
+  write(B), write(' '),
+  evaluate(Expression, Result),
+  write(Result), nl, fail.
+  
