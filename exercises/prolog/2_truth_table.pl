c90abc14f21efeff01ee1bd2b59cb455a04c8db8
sheremetov <semen.sheremetov@bercut.com>
2018-11-25 12:06:12 +0300

    prolog 2 exc & 3 exc

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,19 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+not(A) :- A = fail.
+equ(A, B) :- A = B.
+xor(A, B) :- not(equ(A,B)).
+
+value(X) :- X = true.
+value(X) :- X = fail.
+eval(Expression) :- call(Expression), writeln("true"), !.
+eval(_) :- writeln("fail"), !.
+
+truth_table(A,B,Expression) :-
+    value(A), value(B), write(A), write(" "), write(B), write(" "),
+    eval(Expression), fail.
+truth_table(_, _, _) :- true.
--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,14 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+partition([E|Elist],Y,[E|Hlist],Tlist) :-
+  E =< Y, partition(Elist,Y,Hlist,Tlist).
+partition([E|Elist],Y,Hlist,[E|Tlist]) :-
+  E > Y, partition(Elist,Y,Hlist,Tlist).
+partition([], _,[],[]).
+
+qsort([],[]).
+qsort([E|Elist], K) :-
+  partition(Elist, E, Head, Tail),
+  qsort(Head, Hlist),
+  qsort(Tail, Tlist),
+  append(Hlist, [E|Tlist], K).
