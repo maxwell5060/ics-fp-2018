115e4203adc0bfc46a081dcf5e8da14a5b53fc7b
katterine.rodriguez <katterine.rodriguez@cyxtera.com>
2018-11-26 15:22:10 +0300

    haskell

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,16 @@
 module Hamming (distance) where
 
-distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance :: String -> String -> Int
+distance xs ys = if ((length xs == length ys) && (length xs > 0) && (length ys > 0))
+				then hammingTemp xs ys 0
+				else 0
+
+
+hammingTemp :: String -> String -> Int -> Int
+hammingTemp xs ys cont = if(length xs > 0)
+						then 
+							if (head xs /= head ys)
+							then hammingTemp (tail xs) (tail ys) (cont + 1)
+							else hammingTemp (tail xs) (tail ys) (cont)
+						else
+							cont
