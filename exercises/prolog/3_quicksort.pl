644c13f54040865f1b96bf4c28eeb313fcc2697c
kruchon <krushon96@mail.ru>
2018-11-29 19:41:38 +0300

    lab3 prolog

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,11 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+compareWithPivot(_, [], [], []).
+compareWithPivot(Pivot, [Head|Tail], [Head|LessPart], GreaterPart) :- Pivot >= Head, compareWithPivot(Pivot, Tail, LessPart, GreaterPart). 
+compareWithPivot(Pivot, [Head|Tail], LessPart, [Head|GreaterPart]) :- compareWithPivot(Pivot, Tail, LessPart, GreaterPart).
+
+quicksort([], []).
+quicksort(L, K) :- L = [Head|Tail], compareWithPivot(Head, Tail, LessPart, GreaterPart), 
+    quicksort(LessPart, SortedLessPart), 
+    quicksort(GreaterPart, SortedGreaterPart), 
+    append(SortedLessPart, [Head|SortedGreaterPart], K).
