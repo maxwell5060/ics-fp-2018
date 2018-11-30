072556320cd730293001dfae9462971825b014fa
krasivaya_devochka <demospro1996@gmail.com>
2018-11-30 21:56:01 +0300

    prolog 4

--- exercises/prolog/4_tree.pl
+++ exercises/prolog/4_tree.pl
@@ -1,5 +1,57 @@
-% используя предикат qsort(L,K) из предыдущего задания разработать предикат:
-% balanced_tree(L,T) - который по заданном списку строит сбалансированное бинарное дерево поиска
-% для построения дерева использовать следующие  нотации:
-% empty - пустое дерево 
-% instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+qsort([], []).
+
+
+qsort([Elem], [Elem]).
+
+
+qsort([Pivot|Tail], Sorted):-
+  		divide(Tail, Pivot, BigP, SmallP),
+  		qsort(BigP, SortedBigP),
+  		qsort(SmallP, SortedSmallP),!,
+  		append(SortedSmallP, [Pivot|SortedBigP], Sorted).
+
+
+divide([], _, [], []):-!.
+
+
+divide([Head|Tail], Pivot, [Head|BigP], SmallP):-
+  	Head >= Pivot, !, divide(Tail, Pivot, BigP, SmallP).
+
+
+divide([Head|Tail], Pivot, BigP, [Head|SmallP]):-
+  	divide(Tail, Pivot, BigP, SmallP).
+
+
+balanced_tree([], empty).
+
+
+balanced_tree(List, instant(Head, empty, empty)) :-
+    List = [Head|[]], !.
+
+
+balanced_tree([Lo|[Hi|[]]], instant(Hi, LeftChild, empty)) :-
+    Lo < Hi,
+    balanced_tree([Lo], LeftChild), !.
+
+
+balanced_tree([Hi|[Lo|[]]], instant(Hi, LeftChild, empty)) :-
+    balanced_tree([Lo], LeftChild), !.
+
+
+balanced_tree(List, instant(Mid, LeftChild, RightChild)) :-
+    qsort(List, Sorted),
+    split_by_center(Sorted, LeftList, [Mid|RightList]),
+    balanced_tree(LeftList, LeftChild),
+    balanced_tree(RightList, RightChild), !.
+
+split_by_center(List, Left, Right) :-
+    append(Left, Right, List),
+    length(Left, LeftLength),
+    length(Right, RightLength),
+    (LeftLength =:= RightLength; LeftLength =:= (RightLength - 1)), !.
+
+
+% balanced_tree([9,4,5,6,1,2,3,8], T).
+% instant(5, instant(3, instant(2, instant(1, empty, empty), empty), instant(4, empty, empty)), instant(8, instant(6, empty, empty), instant(9, empty, empty))).
+
+
