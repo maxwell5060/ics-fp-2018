4b8a43574ecf2ee121a94755abbd7985135a741b
Nikita Manakov <nikita.manakov@netcracker.com>
2018-11-30 13:56:04 +0300

    Add Haskell implementation

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,8 @@
 module Hamming (distance) where
 
+dist r [] [] = Just r
+dist r (x:xs) (y:ys) | length xs /= length ys = Nothing
+					 | x == y = dist r xs ys
+					 | x /= y = dist (r + 1) xs ys
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = dist 0 xs ys
