f40db5465a91906dc2470a69bc323ace50d01da4
krasivaya_devochka <demospro1996@gmail.com>
2018-11-30 21:53:19 +0300

    prolog 3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,29 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+
+qsort([], []).
+
+
+qsort([Elem], [Elem]).
+
+
+qsort([Pivot|Tail], Sorted):-
+  		divide(Tail, Pivot, BigP, SmallP),
+  		qsort(BigP, SortedBigP),
+  		qsort(SmallP, SortedSmallP),!,
+  		append(SortedSmallP, [Pivot|SortedBigP], Sorted).
+
+
+divide([], _, [], []):-!.
+
+
+divide([Head|Tail], Pivot, [Head|BigP], SmallP):-
+  	Head >= Pivot, !, divide(Tail, Pivot, BigP, SmallP).
+
+
+divide([Head|Tail], Pivot, BigP, [Head|SmallP]):-
+  	divide(Tail, Pivot, BigP, SmallP).
+
+
+% ?- qsort([2,3,34, 5,1,10,1,3,6], K).
+% K = [1, 1, 2, 3, 3, 5, 6, 10, 34].
+
