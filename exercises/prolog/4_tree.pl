8ee0481be90271edd1d56b0b8297c9e70da8e1f2
Sempai Fedya <44314758+FedorPolevoy@users.noreply.github.com>
2018-12-01 13:46:12 +0300

    task 4 not rdy yet

--- exercises/prolog/4_tree.pl
+++ exercises/prolog/4_tree.pl
@@ -3,3 +3,23 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+% Quicksort
+
+qsort(L, K) :- quicksort(L, K).
+quicksort([], []).
+quicksort([Head|Tail], Sorted) :-
+pivot(Head, Tail, List1, List2),
+quicksort(List1, SortedList1),
+quicksort(List2, SortedList2),
+append(SortedList1, [Head|SortedList2],
+Sorted).
+
+pivot(_, [], [], []).
+
+pivot(Pivot, [Head|Tail], [Head|Left], Right) :- Pivot >= Head, pivot(Pivot, Tail, Left, Right).
+pivot(Pivot, [Head|Tail], Left, [Head|Right]) :- pivot(Pivot, Tail, Left, Right).
+
+% Balanced tree
+
+balanced_tree([], empty).
