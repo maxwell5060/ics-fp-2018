5cf785631525901e1e72bd4d6fb7f7cd7eb76236
EvgenDI <zet09871@yandex.ru>
2018-12-02 15:41:04 +0300

    quicksort

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,10 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+qsort([], []).
+qsort([H|T], R) :- part(H, T, S, B),
+                           qsort(S, L),
+                           qsort(B, G),
+                           append(L, [H|G], R).
+ 
+part(H, [A|T], [A|S], B) :- A =< H, !,part(H, T, S, B).
+part(H, [A|T], S, [A|B]) :- part(H, T, S, B).
+part(_, [], [], []).
