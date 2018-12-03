2075fcafee258762ff4c53865453d2fb0c9e5b36
Duckling Dark <paul.panda.969@gmail.com>
2018-12-03 22:30:21 +0300

    Prolog 1-3 labs

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,26 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+not(A) :- A = fail.
+equ(A, B) :- A = B.
+xor(A, B) :- not(equ(A, B)).
+
+bool(true).
+bool(fail).
+evaluate(E, true) :- E, !.
+evaluate(_, fail).
+
+truth_table(A, B, E) :-
+  bool(A),
+  bool(B),
+  write(A), write(' '),
+  write(B), write(' '),
+  evaluate(E, Result),
+  write(Result), nl, fail.
+% true true true
+% true fail true
+% fail true fail
+% fail fail fail
