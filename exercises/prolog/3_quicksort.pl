8680faa1688f710e913e53abfabfa470e20e3715
Roman <churomann@gmail.com>
2018-12-05 01:45:38 +0300

    Add prolog task 4

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,21 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+qsort([], _).
+qsort(L, K) :-
+    L = [H|T],
+    partition(H, T, Lp, Rp),
+    qsort(Lp, Ls),
+    qsort(Rp, Rs),
+    append(Ls, [H|Rs], K).
+
+partition(_, [], _, _).
+partition(A, L, Lp, Rp) :-
+    L = [H|T],
+    Lp = [H|Tp],
+    A >= H,
+    partition(H, T, Tp, Rp).
+partition(A, L, Lp, Rp) :-
+    L = [H|T],
+    Rp = [H|Tp],
+    A < H,
+    partition(H, T, Lp, Tp).
