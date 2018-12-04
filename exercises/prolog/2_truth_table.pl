c8c8d62633d583c5743393a802eaf4166f6b7c64
v.ivlev <v.ivlev@geoscan.aero>
2018-12-04 09:41:14 +0300

    prolog lab_2

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,74 @@
 % true fail true
 % fail true fail
 % fail fail fail
+and(A,B) :- 
+	A , B.
+
+
+or(A,B) :- 
+	A ; B.
+
+
+not(A):- 
+	A = false.
+
+
+not2(A):- 
+	A, !, false ; true.
+
+
+nand(A,B) :- 
+	not(and(A,B)).
+
+
+nor(A,B) :- 
+	not(or(A,B)).
+
+
+xor(A,B) :- 
+	or(A,B), nand(A,B).
+
+
+equ(A,B) :-
+	 A = B.
+
+
+boolean(true).
+
+
+boolean(false).
+
+
+result(Oper, true) :- 
+		Oper, writeln("true"), !.
+
+
+result(_, false) :-
+		writeln("false"), !.
+
+
+truth_table(A, B, Oper):-
+			boolean(A),
+			boolean(B),
+			write(A), 
+			write(" "), 
+			write(B), 
+			write(" "),
+			result(Oper, _),
+			false.
+
+
+truth_table(_, _, _) :- 
+			true.
+
+
+
+
+% ?- truth_table(A,B,and(A,or(A,B))).
+% true true true
+% true false true
+% false true false
+% false false false
+
+
+
