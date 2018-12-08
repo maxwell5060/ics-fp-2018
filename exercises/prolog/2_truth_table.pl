44b5950391c6bb3e3d5c4e3c040477489daa710e
Maksim Popov <popovmk97@gmail.com>
2018-12-08 16:58:31 +0300

    prolog 2-nd task

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,23 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+	bool(true).
+	bool(fail).
+	and(A,B) :- A, B.
+	or(A,B) :- A; B.
+	not(A) :- \+A.
+	equ(A,B) :- A = B.
+	xor(A,B) :- not(equ(A, B)).
+
+	calc(Expression, true) :- Expression, !.
+	calc(_, fail).
+	row(A, B, Expression, Result) :- bool(A), bool(B), calc(Expression, Result).
+	truth_table(A, B, Expression) :- forall(row(A, B, Expression, Result), (write(A), write(" "), write(B), write(" "), writeln(Result))).
+
+
+	:- truth_table(A,B,and(A,or(A,B))).
+	% true true true
+	% true fail true
+	% fail true fail
+	% fail fail fail
