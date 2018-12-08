bb7a2f1e4fe8172849639f99b0a63a964ce8bc5e
krasivaya_devochka <demospro1996@gmail.com>
2018-12-08 23:26:15 +0300

    scala 1

--- exercises/scala/funsets/src/main/scala/funsets/FunSets.scala
+++ exercises/scala/funsets/src/main/scala/funsets/FunSets.scala
@@ -20,31 +20,31 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
+    def singletonSet(elem: Int): Set = x => x == elem
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set = x => (contains(s, x) || contains(t, x))
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = x => (contains(s, x) && contains(t, x))
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = x  => contains(s, x) && !contains(t, x)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = intersect(s, p)
   
 
   /**
@@ -57,23 +57,30 @@ object FunSets {
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) return true
+      else if (contains(diff(s, p), a))  return false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(bound * -1)
   }
-  
+
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = {
+      def iter(a: Int): Boolean = {
+        if (a > bound) return false
+        else if (contains(intersect(s, p), a))  return true
+        else iter(a + 1)
+      }
+      iter(bound * -1)
+    }
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+    def map(s: Set, f: Int => Int): Set =  x => exists(s, b => f(b) == x)
   
   /**
    * Displays the contents of a set
