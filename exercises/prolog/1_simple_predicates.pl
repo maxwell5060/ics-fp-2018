37636359f7f7cc3d5d79a6eb40ba6ac4fda9ec6e
Maksim Popov <popovmk97@gmail.com>
2018-12-08 15:40:22 +0300

    prolog 1-st task

--- exercises/prolog/1_simple_predicates.pl
+++ exercises/prolog/1_simple_predicates.pl
@@ -10,8 +10,46 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
+
+	brother(X,Y) :- father(Z,X), father(Z,Y), X\=Y.
+	cousin(X,Y) :- father(X1,X), father(Y1,Y), brother(X1,Y1), X\=Y.
+	grandson(X,Y) :- father(Z,X), father(Y,Z).
+	descendent(X,Y) :- father(Y,X).
+	descendent(X,Y) :- father(Y,Z), descendent(X,Z).
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+%	?- brother(X,Y).
+%	?- cousin(X,Y).
+%	?- grandson(X,Y).
+%	?- descendent(X,Y).
+
+:- writeln('brother:').
+:- forall(brother(X,Y), (write(X), writeln(Y))).
+% bc
+% cb
+% de
+% ed
+
+:- writeln('cousin:').
+:- forall(cousin(X,Y), (write(X), writeln(Y))).
+% df
+% ef
+% fd
+% fe
+
+:- writeln('grandson:').
+:- forall(grandson(X,Y), (write(X), writeln(Y))).
+% da
+% ea
+% fa
+
+:- writeln('descendent:').
+:- forall(descendent(X,Y), (write(X), writeln(Y))).
+% ba
+% ca
+% db
+% eb
+% fc
+% da
+% ea
+% fa
