cbee16594271639d3c2bee7c85fd4654110c43a1
agneslovelace <hamman12386@gmail.com>
2018-12-09 22:27:07 +0300

    Done Haskel excercise

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,19 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance [] [] = Just 0
+distance [x] [] = Nothing
+distance [] [x] = Nothing
+distance [x] [y]
+    | x == y = Just 0
+    | otherwise = Just 1
+
+distance (x:xs) (y:ys)
+    | x /= y = 
+        case distance xs ys of
+            Just n  -> Just (n + 1)
+            Nothing -> Nothing
+    | otherwise = 
+        case distance xs ys of
+            Just n  -> Just n
+            Nothing -> Nothing
