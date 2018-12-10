5d9bd83e9ca2022ad706d9e99b17e51f0fbabc4e
Vladislav Kulakovsky <kulakovsky.vladislav@gmail.com>
2018-12-10 17:24:37 +0300

    lab1 done

--- exercises/prolog/1_simple_predicates.pl
+++ exercises/prolog/1_simple_predicates.pl
@@ -11,7 +11,61 @@
 	father(b,e).  % 4
 	father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+brother(Person1, Person2):-
+	father(Dad, Person1),
+	father(Dad, Person2),
+	not(Person1 = Person2).
+
+cousin(Person1, Person2):-
+	father(Dad1, Person1),
+	father(Dad2, Person2),
+	not(Person1 = Person2),
+	brother(Dad1, Dad2).
+
+grandson(Grandson, Grandfather):-
+	father(Grandfather, Son),
+	father(Son, Grandson).
+
+descent(Descendent, Predecessor):-
+	father(Predecessor, Node),
+	descent(Descendent, Node).
+
+descent(Descendent, Predecessor):-
+	father(Predecessor, Descendent).
+
+
+run:-
+	writeln('Brothers:'),
+	forall(brother(Person1, Person2), (write(Person1), write(' '), write(Person2), nl)),
+	writeln('Cousins:'),
+	forall(cousin(Person1, Person2), (write(Person1), write(' '), write(Person2), nl)),
+	writeln('Grandsons:'),
+	forall(grandson(Person1, Person2), (write(Person1), write(' '), write(Person2), nl)),
+	writeln('Descendents:'),
+	forall(descent(Person1, Person2), (write(Person1), write(' '), write(Person2), nl)).
+
+% ?- run.
+% Brothers:
+% b c
+% c b
+% d e
+% e d
+% Cousins:
+% d f
+% e f
+% f d
+% f e
+% Grandsons:
+% d a
+% e a
+% f a
+% Descendents:
+% d a
+% e a
+% f a
+% b a
+% c a
+% d b
+% e b
+% f c
+% true.
