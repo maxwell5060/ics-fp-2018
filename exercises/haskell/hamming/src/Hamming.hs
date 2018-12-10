91e9ef603e00e0f95e52aafa5a025ad9b08b2fa0
Volgarenok <volgarenok@gmail.com>
2018-12-10 17:24:45 -0500

    Haskell Hamming distance

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,11 @@
 module Hamming (distance) where
 
-distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance :: String -> String -> Maybe Integer
+distance str1 str2 = let (f, s) = foldl (\(c, xs) x -> case xs of
+                                           []              -> (Nothing, [])
+                                           (y:ys) | y /= x -> ((+1) <$> c, ys)
+                                           (y:ys)          -> (c, ys)
+                                        ) (Just 0, str1) str2
+                     in case s of
+                        [] -> f
+                        _  -> Nothing
