fe423fa20e4bd1bd43a9fbf2e56226c558aa5c29
Roman <churomann@gmail.com>
2018-12-10 22:51:41 +0300

    fix task 3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,7 +1,7 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
 
 
-qsort([], _).
+qsort([], []).
 qsort(L, K) :-
     L = [H|T],
     partition(H, T, Lp, Rp),
@@ -9,14 +9,14 @@ qsort(L, K) :-
     qsort(Rp, Rs),
     append(Ls, [H|Rs], K).
 
-partition(_, [], _, _).
+partition(_, [], [], []).
 partition(A, L, Lp, Rp) :-
     L = [H|T],
     Lp = [H|Tp],
     A >= H,
-    partition(H, T, Tp, Rp).
+    partition(A, T, Tp, Rp).
 partition(A, L, Lp, Rp) :-
     L = [H|T],
     Rp = [H|Tp],
     A < H,
-    partition(H, T, Lp, Tp).
+    partition(A, T, Lp, Tp).
