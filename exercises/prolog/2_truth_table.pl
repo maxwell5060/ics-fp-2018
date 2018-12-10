4c07b9c9e2fbbd33341b0ca6f64c215c0019fedd
Vladislav Kulakovsky <kulakovsky.vladislav@gmail.com>
2018-12-10 19:28:41 +0300

    lab2 prolog done

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,69 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B):- A, B.
+or(A, B):- A; B.
+or(A,_):- A.
+or(_,B):- B.
+not(A):- \+A.
+equ(A, B):- A = B.
+xor(A, B):- or(and(not(A), B),and(A, not(B))).
+
+boolean(true).
+boolean(false).
+
+truth_table(Expr, true):- Expr, !.
+truth_table(_, false).
+
+truth_table(A, B, Expr):-
+	boolean(A),
+	boolean(B),
+	write(A), write('\t'), write(B), write('\t'), truth_table(Expr, Res), writeln(Res), fail.
+
+truth_table(_,_,_):- nl, true.
+
+test:-
+	writeln('AND:'),
+	truth_table(A, B, and(A,B)),
+	writeln('OR:'),
+	truth_table(A, B, or(A,B)),
+	writeln('EQU:'),
+	truth_table(A, B, equ(A,B)),
+	writeln('XOR:'),
+	truth_table(A, B, xor(A,B)),
+	writeln('and(A,or(A,B))'),
+	truth_table(A,B,and(A,or(A,B))).
+
+% ?- test.
+% AND:
+% true    true    true
+% true    false   false
+% false   true    false
+% false   false   false
+
+% OR:
+% true    true    true
+% true    false   true
+% false   true    true
+% false   false   false
+
+% EQU:
+% true    true    true
+% true    false   false
+% false   true    false
+% false   false   true
+
+% XOR:
+% true    true    false
+% true    false   true
+% false   true    true
+% false   false   false
+
+% and(A,or(A,B))
+% true    true    true
+% true    false   true
+% false   true    false
+% false   false   false
+
+% true.
