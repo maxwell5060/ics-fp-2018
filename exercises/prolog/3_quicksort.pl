ac670929a34bf1a5e1d5bf0a7e2faaf989417259
Volgarenok <volgarenok@gmail.com>
2018-12-10 18:54:37 -0500

    prolog ex 3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,10 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+    qsort(L, K) :- effective_qsort(L, K).
+
+    make_pivot(_, [], [], []).
+    make_pivot(H, [X|T], [X|L], G):- X =< H, make_pivot(H, T, L, G).
+    make_pivot(H, [X|T], L, [X|G]):-  X > H, make_pivot(H, T, L, G).
+
+    effective_qsort(L, S) :- effective_qsort_impl(L, [], S).
+    effective_qsort_impl([], A, A).
+    effective_qsort_impl([H|T], A, S):- make_pivot(H, T, L1, L2), effective_qsort_impl(L1, A, S1), effective_qsort_impl(L2, [H|S1], S).
