1ae5c61d7cfdf01646d5d6546374a822c94f5ae2
Nikita Chistousov <chistousov.nd@gmail.com>
2018-12-11 04:13:58 +0300

    Prolog task 3 completed

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,10 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+partition(P, [H|T], [H|Less], Greater) :- H =< P, partition(P, T, Less, Greater).
+partition(P, [H|T], Less, [H|Greater]) :- H > P, partition(P, T, Less, Greater).
+partition(_, [], [], []).
+
+qsort([H|T], Sorted) :- partition(H, T, Less, Greater), qsort(Less, SortedLess), qsort(Greater, SortedGreater), append(SortedLess, [H|SortedGreater], Sorted).
+qsort([], []).
+
+% qsort([4, 3, 7, 6, 1, 2, 9, 8, 5], X).
+% X = [1, 2, 3, 4, 5, 6, 7, 8, 9]
