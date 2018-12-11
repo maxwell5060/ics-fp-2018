dafc73d6859244ba642f7b6ec859bd08991247ca
Nikita Chistousov <chistousov.nd@gmail.com>
2018-12-11 04:13:49 +0300

    Prolog task 2 completed

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -1,12 +1,64 @@
-% определить предикаты:
-	% and(A,B)
-	% or(A, B)
-	% xor(A, B)
-	% not(A)
-	% equ(A,B)
-% ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
+% Определить предикаты:
+% and(A,B)
+% or(A, B)
+% xor(A, B)
+% not(A)
+% equ(A,B)
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+equ(A, B) :- A = B.
+not(A) :- \+A.
+xor(A, B) :- or(A, B), not(and(A, B)).
+
+evaluate(E, true) :- E, !.
+evaluate(_, false).
+
+bool(true).
+bool(false).
+
+truth_table(A, B, E) :- bool(A), bool(B), evaluate(E, R), write(A), write(' '), write(B), write(' '), write(R), nl, fail.
+truth_table(_, _, _) :- true.
+
+% Использовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
 % truth_table(A,B,and(A,or(A,B))).
 % true true true
 % true fail true
 % fail true fail
 % fail fail fail
+
+truth_table(A, B, and(A, B)).
+% true true true
+% true false false
+% false true false
+% false false false
+
+truth_table(A, B, or(A, B)).
+% true true true
+% true false true
+% false true true
+% false false false
+
+truth_table(A, B, equ(A, B)).
+% true true true
+% true false false
+% false true false
+% false false true
+
+truth_table(A, B, not(equ(A, B))).
+% true true false
+% true false true
+% false true true
+% false false false
+
+truth_table(A, B, xor(A, B)).
+% true true false
+% true false true
+% false true true
+% false false false
+
+truth_table(A, B, and(A, or(A, B))).
+% true true true
+% true false true
+% false true false
+% false false false
