d5eaa2594ee13e776aacfaaf797e60c0c6b276aa
Mikhail Poplavkov <mihail.poplavkov@gmail.com>
2018-12-12 14:18:24 +0300

    Prolog-3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,9 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+less(A, B) :- A < B.
+more(A, B) :- A >= B.
+
+split(E, A, L, R) :- exclude(less(E), A, L), exclude(more(E), A, R).
+
+qsort([H | T], R) :- split(H, T, Left, Right), qsort(Left, NewLeft), qsort(Right, NewRight), append(NewLeft, [H | NewRight], R).
+qsort([], []).
