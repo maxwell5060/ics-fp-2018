8ae394baefce8a6878a1f66da849bc1b25747fd8
v.vasilyev <vasily.vasilev.1997@gmail.com>
2018-12-12 19:52:40 +0300

    lab 7

--- exercises/golang/drawer/config.go
+++ exercises/golang/drawer/config.go
@@ -52,25 +52,25 @@ func DefineConfig() {
 }
 
 func DefineCommandLineConfig() {
-	flag.IntP("width", "w", 500, "width of an image")
+	flag.IntP("width", "w", 16324, "width of an image")
 	viper.BindPFlag("Image.Resolution", flag.Lookup("width"))
 
-	flag.IntP("iterations", "i", 50,
+	flag.IntP("iterations", "i", 128,
 		"iterations to check if P is escaping to infinity")
 	viper.BindPFlag("Algorithm.Iterations", flag.Lookup("iterations"))
 
-	flag.Float32P("scale", "s", 0,
+	flag.Float32P("scale", "s", 0.30,
 		"scale factor to use for projecting pixels onto complex value plane")
 	viper.BindPFlag("Algorithm.ScaleFactor", flag.Lookup("scale"))
 
-	flag.BoolP("parallel", "p", false, "use parallel computations")
+	flag.BoolP("parallel", "p", true, "use parallel computations")
 	viper.BindPFlag("Algorithm.Parallel", flag.Lookup("parallel"))
 
-	flag.Float32P("offset.x", "x", 2.5,
+	flag.Float32P("offset.x", "x", 0,
 		"offset horizontal center point of M on image with")
 	viper.BindPFlag("Image.Offset.X", flag.Lookup("offset.x"))
 
-	flag.Float32P("offset.y", "y", 1.0,
+	flag.Float32P("offset.y", "y", 0,
 		"offset vertical center point of M on image with")
 	viper.BindPFlag("Image.Offset.Y", flag.Lookup("offset.y"))
 
--- exercises/golang/exercise/sample.go
+++ exercises/golang/exercise/sample.go
@@ -1,13 +1,74 @@
 package exercise
 
 import (
+	//	log "github.com/sirupsen/logrus"
 	"github.com/dikderoy/imagen/drawer"
-	log "github.com/sirupsen/logrus"
 	"image/color"
+	"sync"
 )
 
-
-func NewMandelbrot(iterations int, scaleFactor float32, offsetX, offsetY float32) drawer.MandelbrotGenerator {
-	//start here
+type Mandelbrot struct {
+	iterations  int
+	scaleFactor, offsetX, offsetY float32
 }
 
+func NewMandelbrot(iterations int, scaleFactor float32, offsetX, offsetY float32) drawer.MandelbrotGenerator {
+	return Mandelbrot{iterations, scaleFactor, offsetX, offsetY}
+}
+
+func (mandelbrot Mandelbrot) GetColor(X, Y float64) color.Color {
+	x := float64(0)
+	xx := float64(0)
+	y := float64(0)
+	yy := float64(0)
+
+	for i := 0; i < mandelbrot.iterations; i++ {
+		//log.Info("Iteration : i = ", i, "/", mandelbrot.iterations);
+		xx = x*x - y*y + X
+		yy = 2.0*x*y + Y
+		x = xx
+		y = yy
+		if x*x+y*y > 4 {
+			return color.White
+		}
+	}
+
+	return color.Black
+}
+
+func (mandelbrot Mandelbrot) Generate(window *drawer.Image) error {
+	minX := float64(-5)
+	sizeX := float64(10)
+	minY := float64(-5)
+	sizeY := float64(10)
+
+	for x := 0; x < window.Width; x++ {
+		for y := 0; y < window.Height; y++ {
+			X := (minX + float64(x)/float64(window.Width) * sizeX) * float64(mandelbrot.scaleFactor) + float64(mandelbrot.offsetX)
+			Y := (minY + float64(y)/float64(window.Height) * sizeY) * float64(mandelbrot.scaleFactor) + float64(mandelbrot.offsetY)
+			window.Set(x, y, mandelbrot.GetColor(X, Y))
+			//log.Info("Generate : x = ", x, "/", window.Width, " , y = ", y, "/", window.Height, " X = ", X, " Y = ", Y)
+		}
+	}
+	return nil
+}
+
+func (mandelbrot Mandelbrot) GenerateParallel(window *drawer.Image) error {
+	wg := sync.WaitGroup{}
+	minX := float64(-5)
+	sizeX := float64(10)
+	minY := float64(-5)
+	sizeY := float64(10)
+
+	for x := 0; x < window.Width; x++ {
+		for y := 0; y < window.Height; y++ {
+			X := (minX + float64(x)/float64(window.Width) * sizeX) * float64(mandelbrot.scaleFactor) + float64(mandelbrot.offsetX)
+			Y := (minY + float64(y)/float64(window.Height) * sizeY) * float64(mandelbrot.scaleFactor) + float64(mandelbrot.offsetY)
+			wg.Add(1)
+			window.Set(x, y, mandelbrot.GetColor(X, Y))
+			//log.Info("GenerateParallel : x = ", x, "/", window.Width, " , y = ", y, "/", window.Height, " X = ", X, " Y = ", Y)
+			wg.Done()
+		}
+	}
+	return nil
+}
