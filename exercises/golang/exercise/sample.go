43999598fc6ab00a329b471ea7aeca45295e0564
anastasiia.khramtsova <nastya.khramtcova@gmail.com>
2018-12-13 23:27:37 +0300

    go task

--- exercises/golang/exercise/sample.go
+++ exercises/golang/exercise/sample.go
@@ -8,76 +8,52 @@ import (
 	"github.com/dikderoy/imagen/drawer"
 )
 
-// Mandelbrot contains information for performing Mandelbrot calculations
 type Mandelbrot struct {
-	iterations       int
-	scaleFactor      float32
-	offsetX, offsetY float32
+	iterations  int
+	scaleFactor float32
+	offsetX     float32
+	offsetY     float32
 }
 
-func (m Mandelbrot) calculateColor(z complex128) color.Color {
-	var v complex128
-
-	for n := uint8(0); n < uint8(m.iterations); n++ {
-		v = v*v + z
-
-		if cmplx.Abs(v) > 2 {
-			return color.YCbCr{255 - 15*n, 0 + 15*n, 255 - 15*n}
-		}
-	}
-
-	return color.Black
+func NewMandelbrot(iterations int, scaleFactor float32, offsetX, offsetY float32) drawer.MandelbrotGenerator {
+	return Mandelbrot{iterations: iterations, scaleFactor: scaleFactor, offsetX: offsetX, offsetY: offsetY}
 }
 
-// Generate calculates color for each pixel
 func (m Mandelbrot) Generate(canvas *drawer.Image) error {
-	const xmin, ymin, xmax, ymax = -2, -2, +2, +2
-
-	for py := 0; py < canvas.Height; py++ {
-		y := float64(py)/float64(canvas.Height)*(ymax-ymin) + ymin
-
-		for px := 0; px < canvas.Width; px++ {
-			x := float64(px)/float64(canvas.Width)*(xmax-xmin) + xmin
-			z := complex(x, y)
-
-			canvas.Set(px, py, m.calculateColor(z))
+	for iy := 0; iy < canvas.Height; iy++ {
+		y := float64(iy)/float64(canvas.Height)*(-8) + 4
+		for ix := 0; ix < canvas.Width; ix++ {
+			x := float64(ix)/float64(canvas.Width)*(-8) + 4
+			canvas.Set(ix, iy, m.getColour(complex(x, y)))
 		}
 	}
-
 	return nil
 }
 
-// GenerateParallel calculates color for each pixel in parallel
 func (m Mandelbrot) GenerateParallel(canvas *drawer.Image) error {
 	wg := sync.WaitGroup{}
-
-	const xmin, ymin, xmax, ymax = -2, -2, +2, +2
-
-	for py := 0; py < canvas.Height; py++ {
-		y := float64(py)/float64(canvas.Height)*(ymax-ymin) + ymin
-
-		for px := 0; px < canvas.Width; px++ {
-			x := float64(px)/float64(canvas.Width)*(xmax-xmin) + xmin
-			z := complex(x, y)
-
+	for iy := 0; iy < canvas.Height; iy++ {
+		y := float64(iy)/float64(canvas.Height)*(-8) + 4
+		for ix := 0; ix < canvas.Width; ix++ {
+			x := float64(ix)/float64(canvas.Width)*(-8) + 4
 			wg.Add(1)
-
-			go func(px, py int) {
-				canvas.Set(px, py, m.calculateColor(z))
-				wg.Done()
-			}(px, py)
+			canvas.Set(ix, iy, m.getColour(complex(x, y)))
+			wg.Add(1)
 		}
 	}
-
 	return nil
 }
 
-// NewMandelbrot initializes Mandelbrot
-func NewMandelbrot(iterations int, scaleFactor float32, offsetX, offsetY float32) drawer.MandelbrotGenerator {
-	return Mandelbrot{
-		iterations:  iterations,
-		scaleFactor: scaleFactor,
-		offsetX:     offsetX,
-		offsetY:     offsetY,
+func (m Mandelbrot) getColour(xy complex128) color.Color {
+	var f complex128
+	for i := uint8(0); i < uint8(m.iterations); i++ {
+		f = f*f*f + xy
+		if cmplx.Abs(f) > 3 {
+			return color.YCbCr{255 - 10*i, 100 - 2*i, 100}
+		}
+		if cmplx.Abs(f) > 2 && cmplx.Abs(f) <= 3 {
+			return color.YCbCr{255 - 15*i, 0, 100}
+		}
 	}
+	return color.Black
 }
