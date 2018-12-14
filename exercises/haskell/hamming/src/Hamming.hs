89e6060ea672d527eb1ed16996c6247feafa6881
krasivaya_devochka <demospro1996@gmail.com>
2018-12-14 21:10:09 +0300

    haskell

---
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -0,0 +1,6 @@
+module Hamming (distance) where
+
+distance :: String -> String -> Maybe Int
+distance xs ys =
+                if  (length xs == length ys) then Just ( length ( filter id (zipWith (/=) xs ys)))  
+                else error "Nothing, length(xs) != length(ys)"
