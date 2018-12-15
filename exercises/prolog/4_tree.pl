9dabc354500a023735f40b11f84558e0810f3502
EvgenDI <zet09871@yandex.ru>
2018-12-15 21:35:09 +0300

    tree

--- exercises/prolog/4_tree.pl
+++ exercises/prolog/4_tree.pl
@@ -3,3 +3,28 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+balanced_tree([], empty).
+balanced_tree(List, instant(Head, empty, empty)) :- List = [Head|[]], !.
+balanced_tree([L|[H|[]]], instant(H, LeftNode, empty)) :- L < H, balanced_tree([L], LeftNode), !.
+balanced_tree([H|[L|[]]], instant(H, LeftNode, empty)) :- balanced_tree([L], LeftNode).
+balanced_tree(List, instant(Head, LeftNode, RightNode)) :-
+    qsort(List, Sort),
+    split(Sort, LeftList, [Head|RightList]),
+    balanced_tree(RightList, RightNode),
+    balanced_tree(LeftList, LeftNode).
+
+split(List, Left, Right) :-
+    append(Left, Right, List),
+    length(Right, Rightlength),
+    length(Left, Leftlength),
+    (Leftlength =:= Rightlength; Leftlength =:= (Rightlength - 1)).
+
+qsort([], []).
+qsort([H|T], R) :- part(H, T, S, B),
+                           qsort(S, L),
+                           qsort(B, G),
+                           append(L, [H|G], R).
+ 
+part(H, [A|T], [A|S], B) :- A =< H, !,part(H, T, S, B).
+part(H, [A|T], S, [A|B]) :- part(H, T, S, B).
+part(_, [], [], []).	
