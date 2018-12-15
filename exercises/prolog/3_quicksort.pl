9ea96ea5cb0c99c2c9513d0b9a515279418a10a2
kokarovtsevv <44277122+kokarovtsevv@users.noreply.github.com>
2018-12-15 22:47:50 +0300

    prolog lab3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,13 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+append([], Ys, Ys).
+append([X|Xs], Ys, [X|Zs]) :- append(Xs, Ys, Zs).
+
+part([X|Xs], Y, [X|Ls], Bs) :- X=<Y, part(Xs, Y, Ls, Bs).
+part([X|Xs], Y, Ls, [X|Bs]) :- X>Y, part(Xs, Y, Ls, Bs).
+part([],Y,[],[]).
+
+qsort([X|Xs], Ys) :-part(Xs, X, L, B),
+    qsort(L,Ls),
+    qsort(B, Bs),
+    append(Ls, [X|Bs], Ys).
+qsort([],[]).
