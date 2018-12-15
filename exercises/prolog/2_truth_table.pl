1275990e5b97ca5fbdd76c291b13dce60e93abec
vslchnk <vasilchenko_aleksandr@outlook.com>
2018-12-15 22:57:22 +0300

    added second exercise

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,58 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(fail).
+
+and(A,B) :- A, B.
+or(A,B) :- A; B.
+not(A) :- \+A.
+xor(A,B) :- or(and(A,not(B)),and(not(A),B)).
+equ(A,B) :- not(xor(A, B)).
+
+calc(Exp,true) :- Exp, !.
+calc(_,fail).
+
+printTruthTable(A,B,Res) :-
+  write(A),
+  write('\t'),
+  write(B),
+  write('\t'),
+  writeln(Res).
+
+truth_table(A,B,Exp) :-
+  bool(A),
+  bool(B),
+  calc(Exp,Res),
+	printTruthTable(A,B,Res),
+  fail.
+
+% Results:
+%	?- truth_table(A,B,and(A,B)).
+%	true	true	true
+%	true	fail	fail
+%	fail	true	fail
+%	fail	fail	fail
+%	false.
+
+%	?- truth_table(A,B,and(not(A),not(B))).
+%	true	true	fail
+%	true	fail	fail
+%	fail	true	fail
+%	fail	fail	true
+%	false.
+
+%	?- truth_table(A,B,xor(A,not(B))).
+%	true	true	true
+%	true	fail	fail
+%	fail	true	fail
+%	fail	fail	true
+%	false.
+
+%	?- truth_table(A,B,equ(A,not(B))).
+%	true	true	fail
+%	true	fail	true
+%	fail	true	true
+%	fail	fail	fail
+%	false.
