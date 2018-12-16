35c723e40accfa00799917714646d10bf1a18ab4
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-12-16 20:34:31 +0300

    Resolved Prolog task 2 (final version).

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,25 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A,B):- A, B.
+or(A,B):- A | B.
+xor(A,B):- (A,not(B)) | (not(A),B).
+equ(A,B):- A = B.
+not(A):- \+A.
+
+bool(true).
+bool(false).
+
+exec(Exp, true) :- Exp.
+exec(Exp, false):- not(Exp).
+
+truth_table(A, B, Exp) :- forall(
+   (bool(A), bool(B)),
+   (exec(Exp, Res), format("~w\t~w\t~w\n", [A, B, Res]))).
+
+% RESULT:    ?- truth_table(A, B, and(not(A), B)).
+%            true    true    false
+%            true    false   false
+%            false   true    true
+%            false   false   false
