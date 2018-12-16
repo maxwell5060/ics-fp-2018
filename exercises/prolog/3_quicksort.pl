f8069120a62615c2a0b93b5ac87fd0f512b94524
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-12-16 19:47:06 +0300

    Resolved Prolog task 3 (final version).

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,18 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+fork([], _, [], []).
+fork([X| Xs], Pivot, Smalls, Bigs) :-
+    (    X =< Pivot ->
+         Smalls = [X|Rest],
+         fork(Xs, Pivot, Rest, Bigs);
+         Bigs = [X|Rest],
+         fork(Xs, Pivot, Smalls, Rest)).
+qsort([], []).
+qsort([X|Xs], R):-
+    fork(Xs, X, Smaller0, Bigger0),
+    qsort(Smaller0, Smaller),
+    qsort(Bigger0, Bigger),
+    append(Smaller, [X|Bigger], R).
+
+% RESULT:  ?- qsort([1,2,-4,0], L).
+%           L = [-4, 0, 1, 2].
