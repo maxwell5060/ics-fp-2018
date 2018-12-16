e5a8b2790c562ded75b16f663795ef31952a3ea2
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-12-16 14:44:04 +0300

    Resolved Prolog task 1.

--- exercises/prolog/1_simple_predicates.pl
+++ exercises/prolog/1_simple_predicates.pl
@@ -5,13 +5,28 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
+	% father(a,b).  % 1
+	% father(a,c).  % 2
+	% father(b,d).  % 3
+	% father(b,e).  % 4
+	% father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+	% ?- brother(X,Y).
+	% ?- cousin(X,Y).
+	% ?- grandson(X,Y).
+	% ?- descendent(X,Y).
+father(a,b).  % 1
+father(a,c).  % 2
+father(b,d).  % 3
+father(b,e).  % 4
+father(c,f).  % 5
+father(d,i).  % 6
+male(a).
+male(b).
+male(c).
+male(d).
+brother(X,Y):- male(X), father(Z,X), father(Z,Y).
+cousin(X,Y):- father(Z, X), father(Q, Y), brother(Z, Q).
+grandson(X,Y):- male(X), father(Z,X), father(Y, Z).
+descendent(X,Y):- father(Y,X).
+descendent(X,Y):- father(Z,X), descendent(Z,Y).
