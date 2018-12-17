18f96740dea6655e6c7fee0c9d07f0a6058495fd
vszenin <vszenin@tract.ru>
2018-12-17 14:08:08 +0300

    ex 3 prolog

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,15 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+pivot(_, [], [], []).
+
+pivot(Pivot, [Head|Tail], [Head|Left], Right) :- Pivot >= Head, pivot(Pivot, Tail, Left, Right).
+pivot(Pivot, [Head|Tail], Left, [Head|Right]) :- pivot(Pivot, Tail, Left, Right).
+
+qsort(L, K) :- quicksort(L, K).
+quicksort([], []).
+quicksort([Head|Tail], Sorted) :-
+pivot(Head, Tail, List1, List2),
+quicksort(List1, SortedList1),
+quicksort(List2, SortedList2),
+append(SortedList1, [Head|SortedList2],
+Sorted).
