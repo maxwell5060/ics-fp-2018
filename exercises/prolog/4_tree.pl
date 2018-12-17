00ca2cd04d0221ee054d06528522545b022b4b4a
Nikita Chistousov <chistousov.nd@gmail.com>
2018-12-17 04:13:32 +0300

    Prolog task 5 completed

--- exercises/prolog/4_tree.pl
+++ exercises/prolog/4_tree.pl
@@ -3,3 +3,16 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+partition(P, [H|T], [H|Less], Greater) :- H =< P, partition(P, T, Less, Greater).
+partition(P, [H|T], Less, [H|Greater]) :- H > P, partition(P, T, Less, Greater).
+partition(_, [], [], []).
+
+qsort([H|T], Sorted) :- partition(H, T, Less, Greater), qsort(Less, SortedLess), qsort(Greater, SortedGreater), append(SortedLess, [H|SortedGreater], Sorted).
+qsort([], []).
+
+split(List, Left, Right) :- append(Left, Right, List), length(List, Len), Half is Len div 2, length(Left, Half).
+
+general_balanced_tree([], empty).
+general_balanced_tree(List, instant(Root, LeftChild, RightChild)) :- split(List, LeftList, [Root|RightList]), general_balanced_tree(LeftList, LeftChild), general_balanced_tree(RightList, RightChild).
+
+balanced_tree(List, Tree) :- qsort(List, SortedList), general_balanced_tree(SortedList, Tree).
